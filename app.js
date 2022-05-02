7f0c4921f99e2fedbe27aab3905151a6b7ed866a
Mauricio Gael Hernandez Lom <mhernandezl1605@alumno.ipn.mx>
2022-05-02 14:28:23 -0500

    Haciendo los gets, post, put y delete con Express ademas de levantar el servidor

--- app.js
+++ app.js
@@ -15,6 +15,35 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 })
 
+
+
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1, name: "Carlo"}
+    res.status(200).json(explorer)
+})
+
+app.post('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //Parámetros del cliente
+    res.status(201).json({message: "Created"})
+})
+
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parámetros del cliente
+    res.status(200).json({message: "Updated!"})
+})
+
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parámetros del cliente
+    res.status(200).json({message: "Deleted"})
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
